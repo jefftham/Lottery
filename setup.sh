c403b1a33d238f41e638c5767de248faf43d48de
Jeff Tham <jeff.tham@widelity.com>
2016-10-15 13:00:54 -0400

    better control with exporting BASE_dir

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 #this is the main setup script that trigger other all relevant scripts
 
+# $BASE_DIR is the root directory of the repo
+export BASE_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )/" && pwd )
+
 apt-get update
 
 apt-get upgrade -y
@@ -8,6 +11,6 @@ apt-get upgrade -y
 apt-get install -y htop
 
 #install Node Version Manager and NodeJS
-source ./Server_setup/node_setup.sh
+source $BASE_DIR/Server_setup/node_setup.sh
 
 
