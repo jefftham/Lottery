41aaa767f3a3ddf7e9ab18b3b4872e7529dabdee
Jeff Tham <jeff.tham@widelity.com>
2016-10-16 22:00:49 -0400

    cash4life is fully defined

--- Packages/Results/Multistate/cash4life.js
+++ Packages/Results/Multistate/cash4life.js
@@ -5,7 +5,7 @@ var cheerio = require('cheerio');
     
 var  his_url = 'https://data.ny.gov/resource/kwxv-fwze.json';
 
-var live_url = 'https://www.galottery.com/en-us/games/draw-games/cash-for-life.html#tab-winningNumbers';
+var live_url = 'http://www.mdlottery.com/games/cash4life/winning-numbers/';
 
 
 
@@ -58,4 +58,66 @@ module.exports = {
 
         
     }
+
+    
+
+    /**
+     * @function        getLive
+     * @description     This function get the real-time result from the office website.
+     * @access          public
+     * @param  {saveJSON}
+     * @example         var mega = require('./Packages/Results/Multistate/megamillions')
+     *                  var a; mega.getLive(function(live){a = live;});
+     * @author          Jeff Tham <Jeff.Tham@email.com>
+     * @see  {@link http://www.powerball.com/megamillions/mm_numbers.asp}
+     */
+    
+    ,getLive:function(callback){
+
+        //function getLive(callback){
+        var result = {};
+
+        var temp = request(live_url, function(error, response, html){
+            //console.log('response.statusCode ',response.statusCode);
+            //console.log(html);
+            if (!error && response.statusCode == 200) {
+            //console.log(html) ;
+            
+            var $ = cheerio.load(html,{normalizeWhitespace:true, withDomLvl1: true, decodeEntities: true});
+
+            result.date = $('.numbers_tabl tr:nth-of-type(2) td:nth-of-type(1)').text();
+
+            var arr = $('.numbers_tabl tr:nth-of-type(2) strong').first().text().split(' ');
+
+            for(var i =1; i <= 5; i++){
+                result[i] = arr[i - 1];
+            }
+
+            result.cash_ball = $('.numbers_tabl tr:nth-of-type(2) strong').last().text();
+
+
+            callback(result);
+
+            console.log(result);
+           
+          }
+        });
+    }
 };
+
+//other method to request html
+
+/*const https = require('https');
+
+https.get(live_url, (res) => {
+  console.log('statusCode:', res.statusCode);
+  console.log('headers:', res.headers);
+
+  res.on('data', (d) => {
+    process.stdout.write(d);
+  });
+
+}).on('error', (e) => {
+  console.error(e);
+});
+*/
